592473101f7f2909b9e3b5b5dd2440cc764b9d56
Opilane TTHK <opilane@tthk.ee>
2020-02-13 12:00:29 +0200

    ada

--- server.js
+++ server.js
@@ -3,23 +3,22 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req,res){
-    console.log('Hi there from the GET')
+app.get('/', function(req, res){
+    console.log('Heloo');
     console.log(__dirname);
     res.sendFile(__dirname+"/index.html");
 });
 
-
 app.post('/', function(req, res){
-    console.log('Data sent');
-    let num1 = Number(req.body.height);
-    let num2 = Number(req.body.weight);
+    console.log("Data sent");
+    let weight = Number(req.body.Weight);
+    let height = Number(req.body.Height);
 
-    let result = num2/(num1+num2);
+    let result = weight / (height*height);
 
     res.send('Your result is: ' + result);
 });
 
 app.listen(3000, function(){
-    console.log("Server started on port 3000");
-})
+    console.log('Server started on port 3000');
+});
